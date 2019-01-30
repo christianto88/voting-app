2171bb66e684a249ee7b392cacc87eb7ba656519
christianto <christianto88@hotmail.com>
2019-01-30 22:56:36 +0700

    cleanup

--- src/container/VoteContainer.js
+++ src/container/VoteContainer.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Vote from "../component/Vote";
-import {Grid} from '@material-ui/core'
-import {BarChart,CartesianGrid,XAxis,YAxis,Bar,Tooltip,LabelList} from 'recharts'
-function MyBarChart(props){
-  console.log('BC',props)
-return <BarChart width={730} height={250} data={props.data}>
-<CartesianGrid strokeDasharray="3 3" />
-<XAxis dataKey="name" />
-<YAxis />
-<Tooltip />
-<Bar label="value" dataKey="value" fill="#8884d8">
-<LabelList dataKey="value" position="top" />
-</Bar>
-</BarChart>
+import { Grid } from "@material-ui/core";
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Bar,
+  Tooltip,
+  LabelList
+} from "recharts";
+function MyBarChart(props) {
+  return (
+    <BarChart width={730} height={250} data={props.data}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Bar label="value" dataKey="value" fill="#8884d8">
+        <LabelList dataKey="value" position="top" />
+      </Bar>
+    </BarChart>
+  );
 }
 class VoteContainer extends Component {
   vote = async selected => {
@@ -31,27 +40,31 @@ class VoteContainer extends Component {
     let temp = JSON.parse(await localStorage.getItem(event));
     this.setState({ data: temp });
   }
-  chartData=()=>{
-    let data={...this.state.data}
-    delete data.event
-    delete data.endDate
-    let chartData=[]
-    for(let key in data){
-      chartData.push({name:data[key].name,value:data[key].count})
+  chartData = () => {
+    let data = { ...this.state.data };
+    delete data.event;
+    delete data.endDate;
+    let chartData = [];
+    for (let key in data) {
+      chartData.push({ name: data[key].name, value: data[key].count });
     }
-    return chartData
-  }
+    return chartData;
+  };
   render() {
-    const { event,type } = this.props;
-    console.log("render vote container",type);
+    const { event, type } = this.props;
+    console.log("render vote container");
 
-    return type==='finished'?<><Grid item container justify="center" alignItems="center">
-    <Grid item>    
-    <MyBarChart data={this.chartData()}></MyBarChart>
-    </Grid>
-    </Grid></>:(
-     <Vote data={this.state.data} event={event} handleVote={this.vote} />
-    )
+    return type === "finished" ? (
+      <>
+        <Grid item container justify="center" alignItems="center">
+          <Grid item>
+            <MyBarChart data={this.chartData()} />
+          </Grid>
+        </Grid>
+      </>
+    ) : (
+      <Vote data={this.state.data} event={event} handleVote={this.vote} />
+    );
   }
 }
 
